7b787c03b9b6189fd2f691d88277174d8f31cf50
Игорь Петров <igor567-1986@mail.ru>
2022-11-28 13:41:30 +0400

    encoding

--- DataConteiners/Source.cpp
+++ DataConteiners/Source.cpp
@@ -1,8 +1,52 @@
 ﻿#include<iostream>
 using namespace std;
 
+class Element
+{
+	int Data;           //значение элемента
+	Element* pNext;     // Адрес слудующего элемента
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+};
+
+class ForwardList
+{
+	Element* Head; //указатель на начальный элемент списка
+	// Голова является точкой входа в список
+public:
+	ForwardList() :Head(nullptr)
+	{
+		//Конструктор по умолчанию создает пустой список.
+		//Если голова указывает на 0,то и список пуст
+	}
+	~ForwardList()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//                  Adding Elements:
+	void push_front(int Data)
+	{
+		//1)Создаем новый элумент
+		Element* New = new Element(Data);
+		//2)Пристыковываем новый элемент к началу списка
+		New->pNext = Head;
+		//3)Переводим голову на новый элемент
+		Head = New;
+
+	}
+};
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	
+	
 }
