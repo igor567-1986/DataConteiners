fa2359ed3890a77e136561a6457fca67ebe84d58
Игорь Петров <igor567-1986@mail.ru>
2022-12-06 22:19:54 +0400

    ClassWork_06/12_end

--- List2/main.cpp
+++ List2/main.cpp
@@ -1,103 +1,275 @@
+//#include<iostream>
+//using namespace std;
+//
+//#define tab "\t"
+//
+//class Element
+//{
+//	int Data;
+//	Element* pPrev;
+//	Element* pNext;
+//	static unsigned int count;
+//public:
+//
+//	Element( int Data,  Element* pNext = nullptr,Element* pPrev= nullptr):Data(Data), pNext(pNext), pPrev(pPrev)
+//	{
+//		cout << "EConstructor:\t" << this << endl;
+//		count++;
+//	}
+//	~Element()
+//	{
+//		cout << "EDestructor:\t" << this << endl;
+//		count--;
+//	}
+//	friend class DolbyLinkedList;
+//};
+//
+//unsigned int Element::count = 0;
+//
+//class DolbyLinkedList
+//{
+//	Element* Head;
+//	Element* Tail;
+//	unsigned int size;
+//public:
+//	DolbyLinkedList() :Head(nullptr), Tail(nullptr), size(0)
+//	{
+//		cout << "LConstructor:\t" << this << endl;
+//	}
+//	~DolbyLinkedList()
+//	{
+//		cout << "LDestructor:\t" << this << endl;
+//		while (Head)
+//		{
+//			Tail = Head->pNext;
+//			delete Head;
+//			Head = Tail;
+//		}
+//	}
+//	void push_front(int Data)
+//	{
+//		Element* New = new Element(Data);
+//		if (Head == nullptr)
+//		{
+//			Head = New;
+//			Tail = New;
+//		}
+//		else
+//		{
+//			Head->pPrev = New;
+//			New->pNext = Head;
+//			Head = New;
+//		}
+//		size++;
+//	}
+//	void push_back(int Data)
+//	{
+//		Element* New = new Element(Data);
+//		if (Head == nullptr)Head = New;
+//		else
+//		{
+//			Tail->pNext = New;
+//			New->pPrev = Tail;
+//		}
+//		Tail = New;
+//		size++;
+//	}
+//	
+//	void print()const
+//	{
+//	cout << "Head:\t" << Head << endl;
+//	for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+//		cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+//	cout << "________________________________________________" << endl;
+//	cout << "Tail:\t" << Tail << endl;
+//	for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+//		cout << Temp << tab << Temp->Data << tab << Temp->pPrev << endl;
+//	cout << "Колличество элементов списка:\t   " << size << endl;
+//	cout << "Общее колличество элементов списка:" << Element::count << endl;
+//    }    
+//};
+//void main()
+//{
+//	setlocale(LC_ALL, "");
+//	int n;
+//	cout << "Введите размер списка:"; cin >> n;
+//	DolbyLinkedList list;
+//	for (int i=0; i < n; i++)
+//	{
+//		list.push_front(rand() % 100);
+//	}
+//	list.print();
+//
+//}
 #include<iostream>
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define tab "\t"
+#define delimiter "\n----------------------------------\n"
 
-class Element
+class List
 {
-	int Data;
-	Element* pPrev;
-	Element* pNext;
-	static unsigned int count;
-public:
-
-	Element( int Data,  Element* pNext = nullptr,Element* pPrev= nullptr):Data(Data), pNext(pNext), pPrev(pPrev)
+	class Element
 	{
-		cout << "EConstructor:\t" << this << endl;
-		count++;
-	}
-	~Element()
-	{
-		cout << "EDestructor:\t" << this << endl;
-		count--;
-	}
-	friend class DolbyLinkedList;
-};
-
-unsigned int Element::count = 0;
-
-class DolbyLinkedList
-{
-	Element* Head;
-	Element* Tail;
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, * Tail;
 	unsigned int size;
 public:
-	DolbyLinkedList() :Head(nullptr), Tail(nullptr), size(0)
+	List()
 	{
+		Head = Tail = nullptr;
+		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
-	~DolbyLinkedList()
+	List(const std::initializer_list<int>& il) :List()
 	{
-		cout << "LDestructor:\t" << this << endl;
-		while (Head)
-		{
-			Tail = Head->pNext;
-			delete Head;
-			Head = Tail;
-		}
+		for (int const* it = il.begin(); it != il.end(); it++)push_back(it);
 	}
+	List(const List)
+	~List()
+	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//				Adding Elements:
 	void push_front(int Data)
 	{
-		Element* New = new Element(Data);
-		if (Head == nullptr)
+		if (Head == nullptr && Tail == nullptr)
 		{
-			Head = New;
-			Tail = New;
+			Head = Tail = new Element(Data);
+			size++;
+			return;
 		}
-		else
-		{
-			Head->pPrev = New;
-			New->pNext = Head;
-			Head = New;
-		}
+		Head = Head->pPrev = new Element(Data, Head);
 		size++;
 	}
 	void push_back(int Data)
 	{
-		Element* New = new Element(Data);
-		if (Head == nullptr)Head = New;
-		else
+		if (Head == nullptr && Tail == nullptr)
 		{
-			Tail->pNext = New;
-			New->pPrev = Tail;
+			Head = Tail = new Element(Data);
+			size++;
+			return;
 		}
-		Tail = New;
+		Tail = Tail->pNext = new Element(Data, nullptr, Tail);
 		size++;
 	}
-	
+	void insert(int Index, int Data)
+	{
+		if (Index > size)
+		{
+			cout << "Eror: Out of range" << endl;
+			return;
+		}
+		if (Index == 0)return push_front(Data);
+		if (Index == size)return push_back(Data);
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (int i = 0; i < size - Index-1; i++)Temp = Temp->pPrev;
+		}
+	/*	Element* New = new Element(Data);
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;
+		size++;*/
+		Temp->pPrev = Temp->pPrev->pNext = new Element(Data,Temp,Temp->pPrev);
+		size++;
+	}
+	//				Removing Elements:
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		Head = Head->pNext;
+		delete Head->pPrev;
+		Head->pPrev = nullptr;
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == Tail)return pop_front();
+		Tail = Tail->pPrev;
+		delete Tail->pNext;
+		Tail->pNext = nullptr;
+		size--;
+	}
+
+	//				Methods:
 	void print()const
 	{
-	cout << "Head:\t" << Head << endl;
-	for (Element* Temp = Head; Temp; Temp = Temp->pNext)
-		cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
-	cout << "________________________________________________" << endl;
-	cout << "Tail:\t" << Tail << endl;
-	for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
-		cout << Temp << tab << Temp->Data << tab << Temp->pPrev << endl;
-	cout << "Колличество элементов списка:\t   " << size << endl;
-	cout << "Общее колличество элементов списка:" << Element::count << endl;
-    }    
+		cout << "Голова списка: " << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Хвост списка: " << Tail << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+	void reverse_print()const
+	{
+		cout << "Хвост списка: " << Tail << endl;
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Голова списка: " << Head << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
 };
+
+//#define BASE_CHECK
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
 	int n;
-	cout << "Введите размер списка:"; cin >> n;
-	DolbyLinkedList list;
-	for (int i=0; i < n; i++)
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
 	list.print();
+	list.reverse_print();
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента:"; cin >> index;
+	cout << "Введите значение добавляемого элемента:"; cin >> value;
+	list.insert(index, value);
+	list.print();
+	list.reverse_print();
+#endif // BASE_CHECK
+
+	List list = { 3,5,8,13,21 };
+	list.print();
+	list.reverse_print();
 
 }
